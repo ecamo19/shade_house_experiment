363b72a57bc0bf512e64213c827d92ab83b51656
ecamo19 <erickcamo19@gmail.com>
2020-11-25 14:50:31 -0600

    RGR and AGR funtion calculator added

---
+++ codes/agr_rgr_funtions_calculators.R
@@ -0,0 +1,59 @@
+# Packages ----------------------------------------------------------------
+library(docstring)
+
+
+# Function RGR ------------------------------------------------------------
+
+rgr <- function(height, days){
+	#' This funtion calculates the Relative growth rate 
+	#' (log(Final Height) - log(Inital height)) /
+	#'           time final - time initial  
+	
+	#Select the first mesaure
+	initial_heigth <- height[days == 0]
+	
+	#Calculate rgr
+	rgr <- ( log(height) - log(initial_heigth)) / days
+	return(rgr)
+}
+
+
+# Function AGR ------------------------------------------------------------
+
+agr <- function(height, days){
+	#' This funtion calculates the absulate growth rate 
+	#'         (Final Height - Inital height) /
+	#'           time final - time initial  
+	
+	#Select the first mesaure
+	initial_heigth <- height[days == 0]
+	
+	#Calculate rgr
+	agr <- ( height - initial_heigth) / days
+	return(agr)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
